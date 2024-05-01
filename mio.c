52bab51cb9e58bbfc1ea3404bcf56543d1f0a23d
Programador Novato <info@programadornovato.com>
2024-05-01 16:36:27 -0600

    16.- Operador condicional if else
    Declaramos e inicializamos una variable.
    Pedimos al humano que ingrese un numero.
    Si el numero ingresado es igual al numero inicializado felicitamos al humano de lo contrario no.

--- mio.c
+++ mio.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	float x,y,res;
-	printf("Humano ingresa el valor de x:");
-	scanf("%f",&x);
-	printf("Humano ingresa el valor de y:");
-	scanf("%f",&y);
-	res= (sqrt(x)) / (pow(y,2)-1);
-	printf("Humano aqui esta tu chingadera:%.2f\n",res);
+	int numeroEncontrar=5,numeroHumano;
+	
+	printf("Humano ingresa el numero que estoy pensando\n");
+	scanf("%d",&numeroHumano);
+	if(numeroHumano==numeroEncontrar){
+		printf("Bien jugado humano\n");
+	}
+	else{
+		printf("Humano tonto\n");
+	}
 	system("pause");
 	return 0;
 
