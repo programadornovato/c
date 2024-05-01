a5e21ba0271776706f4be85ffb4f24fb156b0617
Programador Novato <info@programadornovato.com>
2024-05-01 10:21:18 -0600

    14.- Ejercicio. Calcular calificación de un alumno
    Calificación de prácticas que es 40%.
    Calificación de participación que es 20%.
    Calificación de examen que es 40%.
    Obtener la calificación final sumando y obteniendo el promedio.

--- mio.c
+++ mio.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 int main(){
-	int a,b,aux;
-	printf("Humano ingresa estos valores:\n");
-	printf("Valor de a:");
-	scanf("%d",&a);
-	printf("Valor de b:");
-	scanf("%d",&b);
-	//aux=10
-	aux=a;
-	//a=5
-	a=b;
-	//b=10
-	b=aux;
-	printf("Valor de a:%d\n",a);
-	printf("Valor de b:%d\n",b);
+	float practicas,participacion,examen,calFinal;
+	printf("Humano ingresa los siguientes datos:\n");
+	printf("practicas:");
+	scanf("%f",&practicas);
+	printf("participacion:");
+	scanf("%f",&participacion);
+	printf("examen:");
+	scanf("%f",&examen);
+	
+	practicas=practicas*.40;
+	participacion=participacion*.20;
+	examen=examen*.40;
+	
+	calFinal=practicas+participacion+examen;
+	
+	printf("Humano esta es tu calificacion:%.2f\n",calFinal);
+	
+	
 	system("pause");
 	return 0;
 
