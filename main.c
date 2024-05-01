67c300cdbe8dcaaacd0c402d8f6e792430d457a8
Programador Novato <info@programadornovato.com>
2024-05-01 02:44:05 -0600

    07 Función scanf para introducir números
    Agregamos la libreria <stdlib.h>
    Declarar variables
    Ingresamos un numero

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main2(int argc, char *argv[]) {
 	printf("Hola mundo!!!!");
 	getchar();
 	return 0;
--- mio.c
+++ mio.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
-	int entero=10.5;
-	float flotante=5.5;
-	double doble=55.6666;
-	char c='c';
-	printf("Entero=%d\n",entero);
-	printf("Flotante=%f\n",flotante);
-	printf("Doble=%lf\n",doble);
-	printf("Caracter=%c\n",c);
+	int entero;
+	printf("Humano ingresa un numero entero:");
+	scanf("%d",&entero);
+	printf("Humano aqui esta el numero entero que metiste:%d\n",entero);
+	float flotante;
+	printf("Humano ingresa un numero flotante:");
+	scanf("%f",&flotante);
+	printf("Humano aqui esta el numero flotante que metiste:%f\n",flotante);
+	//getch();
+	system("pause");
 	return 0;
 }
 
