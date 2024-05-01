87908ea3974792271bf7baf9cdbb43ec4e872a85
Programador Novato <info@programadornovato.com>
2024-05-01 03:40:45 -0600

    08 Comentarios y buenas practicas en comentarios
    Tipos de comentarios.
    Buenas practicas
    	Comentar con Propósito.
    	Comentarios Actualizados.
    	Comentarios para Seccionar el Código.
    	Evitar Comentarios Excesivos.
    	Comentarios para TO-DO.
    	Explicar Decisiones Complejas

--- mio.c
+++ mio.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
 int main(){
-	int entero;
-	printf("Humano ingresa un numero entero:");
-	scanf("%d",&entero);
-	printf("Humano aqui esta el numero entero que metiste:%d\n",entero);
-	float flotante;
-	printf("Humano ingresa un numero flotante:");
-	scanf("%f",&flotante);
-	printf("Humano aqui esta el numero flotante que metiste:%f\n",flotante);
-	//getch();
+	// Esto es un comentario de una sola línea
+	int x = 5; // Esto también es un comentario, al final de una línea de código
+
+	/* 
+	Este es un comentario
+	de múltiples líneas.
+    Puede abarcar varias líneas. 
+    */
+
+	// Inicia la calificacion en 5 porque todos van a tener 5 al inicio
+	int calificacion = 5; 
+	// asigna 1 a contador
+	int contador = 1;
+
+	// El diametro inicial del circulo es de 20cm y aumentara hasta 20
+	float moneda=5.5;
+	
+	// Inicialización de variables
+    int contador = 0;
+    double calificacion = 0.0;
+    // Asignacion de variables
+    printf("Ingrese un valor entero para 'contador': ");
+    scanf("%d", &contador);
+    printf("Ingrese un valor double para 'calificacion': ");
+    scanf("%lf", &calificacion);
+
+	// Inicialización de variables
+    int contador = 0;
+    double calificacion = 0.0;
+	// TODO: Crear la asignacion de variables
+	
+
+	/* 
+	Se utiliza un factor de corrección de 1.1 para ajustar la discrepancia entre
+   	las mediciones experimentales y los valores teóricos observados en las pruebas preliminares. 
+	*/
+	double correctionFactor = 1.1;
+
+
+
+
 	system("pause");
 	return 0;
+
 }
 
