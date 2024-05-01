cc63b2a504be971f83923647d507bfa531b4b355
Programador Novato <info@programadornovato.com>
2024-05-01 00:59:39 -0600

    03.- getchar

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int otra(int argc, char *argv[]) {
+
+int main(int argc, char *argv[]) {
 	printf("Hola mundo!!!!");
 	getchar();
 	return 0;
 }
+
