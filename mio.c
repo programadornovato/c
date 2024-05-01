1110fdfd60cb82ec44759064060acfffeb642af0
Programador Novato <info@programadornovato.com>
2024-05-01 01:00:58 -0600

    04.-Crear un programa
    Crear un archivo desde 0.
    Llamar una libreria.
    Crear la función main
    Imprimir texto.
    Detener la pantalla.

---
+++ mio.c
@@ -0,0 +1,13 @@
+/*Mi primer programa desde 0 */
+#include <stdio.h>
+int main(){
+	printf("Soy un cosa pero barbara");
+	getchar();
+	printf("Verdad?");
+	getchar();
+	printf("Tu que crees?");
+	getchar();
+	printf("Pero dime algo");
+	return 0;
+}
+
