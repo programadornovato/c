f0cdc9feadedb6340d6b73a4cfd410834c0867d2
Programador Novato <info@programadornovato.com>
2024-05-01 06:49:53 -0600

    11.- Ejercicio Expresiones matemáticas
    Convertir una expresión matemática en código.
    Redondear números decimales a mostrar.

--- mio.c
+++ mio.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 int main(){
-	char sexo[10];
-	int edad;
-	float estatura;
-	
-	printf("Humano dame tu sexo:");
-	scanf("%s",sexo);
-	printf("Humano dame tu edad:");
-	scanf("%d",&edad);
-	printf("Humano dame tu estatura:");
-	scanf("%f",&estatura);
-	
-	printf("HUMANO AQUI ESTAN TUS PINCHES DATOS\n");
-	printf("Sexo:%s\n",sexo);
-	printf("Edad:%d\n",edad);
-	printf("Estatura:%f\n",estatura);
-	
+	float a,b,res;
+	printf("Humano por favor ingrea estos datos:\n");
+	printf("Valor de a:");
+	scanf("%f",&a);
+	printf("Valor de b:");
+	scanf("%f",&b);
+	//exp,*,/,+,-
+	res=a/b+1;
+	printf("Aqui esta tu pinche resultado %.2f\n",res);
 	system("pause");
 	return 0;
 
