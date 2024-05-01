f7867a2ce54916d7c181e939f63958233aa24ec7
Programador Novato <info@programadornovato.com>
2024-05-01 04:38:51 -0600

    09 Operadores aritmeticos
    Declarar múltiples variables en una línea.
    Pedir al usuario que ingrese el valor de 2 variables
    Realizar varias operaciones aritméticas (Suma,Resta, Division y Multiplicacion).
    Mostramos resultados.

--- mio.c
+++ mio.c
@@ -1,46 +1,24 @@
 #include <stdio.h>
 int main(){
-	// Esto es un comentario de una sola línea
-	int x = 5; // Esto también es un comentario, al final de una línea de código
-
-	/* 
-	Este es un comentario
-	de múltiples líneas.
-    Puede abarcar varias líneas. 
-    */
-
-	// Inicia la calificacion en 5 porque todos van a tener 5 al inicio
-	int calificacion = 5; 
-	// asigna 1 a contador
-	int contador = 1;
-
-	// El diametro inicial del circulo es de 20cm y aumentara hasta 20
-	float moneda=5.5;
+	float n1,n2,suma,resta,mult,div;
 	
-	// Inicialización de variables
-    int contador = 0;
-    double calificacion = 0.0;
-    // Asignacion de variables
-    printf("Ingrese un valor entero para 'contador': ");
-    scanf("%d", &contador);
-    printf("Ingrese un valor double para 'calificacion': ");
-    scanf("%lf", &calificacion);
-
-	// Inicialización de variables
-    int contador = 0;
-    double calificacion = 0.0;
-	// TODO: Crear la asignacion de variables
+	printf("Humano ingresa el valor 1:");
+	scanf("%f",&n1);
+	printf("Humano ingresa el valor 2:");
+	scanf("%f",&n2);
 	
-
-	/* 
-	Se utiliza un factor de corrección de 1.1 para ajustar la discrepancia entre
-   	las mediciones experimentales y los valores teóricos observados en las pruebas preliminares. 
-	*/
-	double correctionFactor = 1.1;
-
-
-
-
+	suma=n1+n2;
+	printf("Humano aqui esta tu pich suma:%f\n",suma);
+	
+	resta=n1-n2;
+	printf("Humano aqui esta tu pich resta:%f\n",resta);
+	
+	mult=n1*n2;
+	printf("Humano aqui esta tu pich multiplicacion:%f\n",mult);
+	
+	div=n1/n2;
+	printf("Humano aqui esta tu pich divicion:%f\n",div);
+	
 	system("pause");
 	return 0;
 
