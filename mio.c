de39f7d60b8be31ec16b5ec22c61fd817fd053e4
Programador Novato <info@programadornovato.com>
2024-05-03 10:02:35 -0600

    18.- Switch Case
    La sentencia switch es lo mismo que if pero más sencillo.

--- mio.c
+++ mio.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	float x,y,res,yAlcuadradoMenos1;
-	ingresaNuevo:
-	printf("Humano ingresa el valor de x:");
-	scanf("%f",&x);
-	printf("Humano ingresa el valor de y:");
-	scanf("%f",&y);
-	yAlcuadradoMenos1=(pow(y,2)-1);
-	if(yAlcuadradoMenos1==0){
-		printf("Humano estupido y al cuadrado menos 1 no puede ser 0 intentalo de nuevo\n");
-		goto ingresaNuevo;
+	int valor;
+	regresar:
+	printf("Humano escribe un numero entre 1 y 5\n");
+	scanf("%d",&valor);
+	switch(valor){
+		case 1:
+			printf("Precionaste el numero uno\n");
+			break;
+		case 2:
+			printf("Precionaste el numero dos\n");
+			break;
+		case 3:
+			printf("Precionaste el numero tres\n");
+			break;
+		case 4:
+			printf("Precionaste el numero cuatro\n");
+			break;
+		case 5:
+			printf("Precionaste el numero cinco\n");
+			break;
+		default:
+			printf("Humano estupido te pedi un numero entre 1 y 5\n");
+			goto regresar;
+			break;
 	}
-	else{
-		res= (sqrt(x)) / yAlcuadradoMenos1;
-		printf("Humano aqui esta tu chingadera:%.2f\n",res);
-	}
+	
+	
+	
 	system("pause");
 	return 0;
 
