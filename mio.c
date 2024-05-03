0581a476f9ae5d6bf789ba660cfc2f576b4aee0c
Programador Novato <info@programadornovato.com>
2024-05-03 17:59:24 -0600

    21.- Determinar si es una vocal o no
    Pedir al humano que ingrese una vocal.
    Determinar se es vocal con switch case

--- mio.c
+++ mio.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	int numero;
-	printf("Humano escribe un numero entero\n");
-	scanf("%d",&numero);
-	if(numero%2==0){
-		printf("El numero es par\n");
+	char letra;
+	regresa:
+	printf("Humano ingresa una letra vocal\n");
+	scanf("%c",&letra);
+	switch(letra){
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+			printf("Bien humano\n");
+			break;
+		default:
+			printf("Humano tonto te pedi una vocal\n");
+			goto regresa;
+			break;
 	}
-	else{
-		printf("El numero es impar\n");
-	}
+	
 	system("pause");
 	return 0;
 
