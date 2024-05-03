d20bc07c738c0c7bb7e4a8c5a9ac959d87fc73c1
Programador Novato <info@programadornovato.com>
2024-05-03 09:39:43 -0600

    17.- Validar un numero con la condicional if
    Validamos que el dividendo no sea 0 para evitar valores infinitos.
    Usaremos la función goto para regresar al usuario a ingresar el valor de y.

--- mio.c
+++ mio.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	int numeroEncontrar=5,numeroHumano;
-	
-	printf("Humano ingresa el numero que estoy pensando\n");
-	scanf("%d",&numeroHumano);
-	if(numeroHumano==numeroEncontrar){
-		printf("Bien jugado humano\n");
+	float x,y,res,yAlcuadradoMenos1;
+	ingresaNuevo:
+	printf("Humano ingresa el valor de x:");
+	scanf("%f",&x);
+	printf("Humano ingresa el valor de y:");
+	scanf("%f",&y);
+	yAlcuadradoMenos1=(pow(y,2)-1);
+	if(yAlcuadradoMenos1==0){
+		printf("Humano estupido y al cuadrado menos 1 no puede ser 0 intentalo de nuevo\n");
+		goto ingresaNuevo;
 	}
 	else{
-		printf("Humano tonto\n");
+		res= (sqrt(x)) / yAlcuadradoMenos1;
+		printf("Humano aqui esta tu chingadera:%.2f\n",res);
 	}
 	system("pause");
 	return 0;
