3cfee1ac84d246d1ea650e0eedd90e77d13db270
Programador Novato <info@programadornovato.com>
2024-05-06 17:48:35 -0600

    29.-Ejercicio Sumar 1+2+3 hasta n
    Hacer un programa que:
    Programa que pida un número entero n.
    Validar que el número n sea mayor a 1.
    El programa hará un ciclo que sume los números 1+2+3 hasta n.

--- mio.c
+++ mio.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
-	int numero;
+	int n;
 	do{
-		printf("Humano escribe un numero entre 1 y 10:");
-		scanf("%d",&numero);
-		if(numero<=0 || numero>10){
-			printf("Humano estupido te pedi un numero entre 1 y 10\n");
-		}
-	}while(numero<=0 || numero>10);
-	printf("\n\n");
-	int i;
-	for(i=0;i<10;i++){
-		printf("El valor de %d x %d es %d\n",i+1,numero,(i+1)*numero);
+		printf("Humano dame un numero mayor a 1:");
+		scanf("%d",&n);
+	}while(n<=1);
+	int i,suma=0;
+	for(i=0;i<n;i++){
+		printf("%d+",(i+1));
+		suma=suma+(i+1);
 	}
+	printf("\nLa suma es:%d\n",suma);
 	
 	system("pause");
 	return 0;
