7c9b5ae5a90d1f6e071051d3aeefd174078874a0
Programador Novato <info@programadornovato.com>
2024-05-06 16:51:18 -0600

    27.-  Ejercicio.Tablas de multiplicar
    Programa que pida al humano que digite  un número entre 1 y 10 y que valide que este dentro el rango de 1 y 10.
    Mostrar los múltiplos de  de ese número del 1 al 10.

--- mio.c
+++ mio.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 int main(){
+	int numero;
+	do{
+		printf("Humano escribe un numero entre 1 y 10:");
+		scanf("%d",&numero);
+		if(numero<=0 || numero>10){
+			printf("Humano estupido te pedi un numero entre 1 y 10\n");
+		}
+	}while(numero<=0 || numero>10);
+	printf("\n\n");
 	int i;
-	for(i=10;i>0;i=i-2){
-		printf("%d\n",i);
+	for(i=0;i<10;i++){
+		printf("El valor de %d x %d es %d\n",i+1,numero,(i+1)*numero);
 	}
+	
 	system("pause");
 	return 0;
 
