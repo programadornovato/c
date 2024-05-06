fbf88642a51fad0d997e22eaaeca6799f6db6d87
Programador Novato <info@programadornovato.com>
2024-05-06 10:25:04 -0600

    23.- Ejercicio Sistema bancario
    Que aremos
    Hacer un programa que simule un banco con un saldo inicial de 1000 Dólares.
    
    Qué Aprenderemos:
    Manejo de switch y if de forma simultánea.

--- mio.c
+++ mio.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
-#include <math.h>
 int main(){
-	int edad;
-	printf("Humano dame la edad del postulante:");
-	scanf("%d",&edad);
-	if(edad >= 18 && edad <= 40 ){
-		printf("La persona es apta para trabajar en nuestra empresa\n");
-	}else{
-		printf("La persona no es apta para trabajar en nuestra empresa\n");
+	int opcion;
+	float saldo=1000,ingreso,sacar;
+	printf("Hola humano, estas en el banco jerte\n");
+	regreso:
+	printf("Ingresa una opcion\n");
+	printf("Opcion 1. Ingreso de dinero\n");
+	printf("Opcion 2. Sacar de dinero\n");
+	printf("Opcion 3. Salir\n");
+	
+	scanf("%d",&opcion);
+	
+	switch(opcion){
+		case 1:
+			printf("Humano cuanto dinero vas a ingresar:");
+			scanf("%f",&ingreso);
+			saldo=saldo+ingreso;
+			printf("Humano tu cuenta es de %.2f\n",saldo);
+			goto regreso;
+		case 2:
+			printf("Humano cuanto dinero vas a sacar:");
+			scanf("%f",&sacar);
+			if(sacar>saldo){
+				printf("Humano pillin no puedes sacar mas dinero de que ya tienes\n");
+				goto regreso;
+			}else{
+				saldo=saldo-sacar;
+				printf("Humano tu cuenta es de %.2f\n",saldo);
+				goto regreso;
+			}
+		case 3:
+			printf("Adios humano\n");
+			break;
+		default:
+			goto regreso;
 	}
 	
 	system("pause");
