298e0bae2bb8a92c1ed317254cab3c59f15e989c
Programador Novato <info@programadornovato.com>
2024-05-07 17:27:12 -0600

    36.-Numero Mayor del arreglo
    En este ejercicio vamos a:
    Pedir al humano que ingrese la cantidad de elementos.
    Pedir al humano que ingrese el valor de cada elemento.
    Buscar el elemento mayor del arreglo.

--- mio.c
+++ mio.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 int main(){
-	int i,n,arreglo[100];
+	int i,n,mayor=0,arreglo[100];
 	do{
-		printf("Humano ingresa la cantidad de elementos (maximo 100):");
+		printf("Humano ingresa el tamaño del arreglo (maximo de 100):");
 		scanf("%d",&n);
 	}while(n>100);
 	for(i=0;i<n;i++){
-		printf("\nHumano ingresa el valor %d\n",(i+1));
+		printf("\nIngresa el valor %d:",(i+1));
 		scanf("%d",&arreglo[i]);
 	}
+	//3 4 5 1 2
 	for(i=0;i<n;i++){
-		printf("Indice=%d valor %d\n",(i+1),arreglo[i]);
+		if(arreglo[i]>mayor){
+			mayor=arreglo[i];
+		}
 	}
+	printf("\nHumano aqui esta el numero mayor:%d\n",mayor);
+	
 	
 	system("pause");
 	return 0;
