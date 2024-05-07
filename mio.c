7997a2b0a6e4ebaa3215b751a470d2a5786f2cfe
Programador Novato <info@programadornovato.com>
2024-05-07 16:04:59 -0600

    34.- Suma de números de un arreglo
    Definir los elementos de un arreglo.
    Sume cada elemento del arreglo.
    Mostrar la suma.

--- mio.c
+++ mio.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-#include <math.h>
-#include <time.h>
 int main(){
-	int n,aleatorio,contador=0;
-	srand(time(NULL));
-	aleatorio= 1 + rand() % 100;
-	do{
-		printf("Humano que numero estoy pensando entre 1 y 100 (%d):",aleatorio);
-		scanf("%d",&n);
-		if(n<aleatorio){
-			printf("Humano estupido tu numero es menor\n");
-		}
-		if(n>aleatorio){
-			printf("Humano estupido tu numero es mayor\n");
-		}
-		contador++;
-	}while(n!=aleatorio);
-	printf("Felicidades Humano pero te equivocaste %d veces\n",contador);
-	
+	int suma=0,i,arreglo[]={10,9,8,7,6};
+	for(i=0;i<5;i++){
+		suma=suma+arreglo[i];
+		printf("%d+",arreglo[i]);
+	}
+	printf("\nLa suma es %d\n",suma);
 	system("pause");
 	return 0;
 
