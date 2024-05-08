7c6ec9ea9b41651299906dc43ac95f73ef0e3b01
Programador Novato <info@programadornovato.com>
2024-05-08 15:16:04 -0600

    46.- Comparar string con strcmp
    En este ejercicio vamos:
    Definir dos strings.
    Mostrar si:
    Son iguales.
    O diferentes.
    Cual de los dos es mayor.

--- mio.c
+++ mio.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int main(){
-	int longitud=0;
-	char texto[11];
-	regresa:
-	printf("Humano ingresa un texto de minimo 10 letras:");
-	fgets(texto,sizeof(texto),stdin);
-	texto[strcspn(texto, "\n")] = 0;
-	longitud=strlen(texto);
-	if(longitud>=10){
-		printf("Bien hecho humano, ahora dame la patita\n");
+	char texto1[]="A";
+	char texto2[]="A";
+	int res;
+	res=strcmp(texto1,texto2);
+	//printf("res=%d\n",res);
+	if(res==0){
+		printf("Ambos dos textos son iguales\n");
 	}else{
-		printf("Humano tonto\n");
-		goto regresa;
+		if(res>0){
+			printf("El texto1 es mayor\n");
+		}
+		if(res<0){
+			printf("El texto1 es menor\n");
+		}
 	}
 	system("pause");
 	return 0;
