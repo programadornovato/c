d9860cc74dcffa78c2d722677dd6685244ccf004
Programador Novato <info@programadornovato.com>
2024-05-07 20:59:44 -0600

    40.- Copiar el contenido de una matriz a otra matriz
    En este ejercicio vamos:
    Pedirle al humano que ingrese las filas y las columnas de la matriza.
    Hacer un ciclo anidado con las filas y columnas para que el humano ingrese los datos de la matriza.
    Copiar el contenido de la matriza a la matrizb.
    Mostrar el contenido de la matrizb

--- mio.c
+++ mio.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 int main(){
-	int i,j,matriz[100][100],filas,columnas;
-	printf("Humano cuantas filas quieres:");
-	scanf("%d",&filas);
-	printf("Humano cuantas columnas quieres:");
-	scanf("%d",&columnas);
+	int i,j,matriza[100][100],matrizb[100][100],filas,columnas;
+	printf("Humano ingres alas filas y presiona enter y despues las columnas y preciona enter:");
+	scanf("%d %d",&filas,&columnas);
 	for(i=0;i<filas;i++){
-		for(j=0;j<filas;j++){
-			printf("Valor de [%d][%d]:",i+1,j+1);
-			scanf("%d",&matriz[i][j]);
+		for(j=0;j<columnas;j++){
+			printf("[%d][%d]",i+1,j+1);
+			scanf("%d",&matriza[i][j]);
 		}		
 	}
-	printf("\n\n");
+	printf("\n\n");	
 	for(i=0;i<filas;i++){
-		for(j=0;j<filas;j++){
-			printf("[%d]",matriz[i][j]);
+		for(j=0;j<columnas;j++){
+			matrizb[i][j]=matriza[i][j];
+		}		
+	}
+	printf("\n\n");	
+	for(i=0;i<filas;i++){
+		for(j=0;j<columnas;j++){
+			printf("[%d]",matrizb[i][j]);
 		}
 		printf("\n");
 	}
-	printf("\n\n");
+	
 	system("pause");
 	return 0;
 
