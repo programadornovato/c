88aee0e97f774cbd2ddae998f80a0d46722cea5a
Programador Novato <info@programadornovato.com>
2024-05-08 02:30:40 -0600

    41.- Matriz simétrica
    Una matriz es simétrica si es una matriz cuadrada, la cual tiene la característica de ser igual su transpuesta.
    
    En este ejercicio vamos:
    Inicializamos la matriz simétrica.
    Comprobamos si es simetrica.
    Mostramos un mensaje si es simetrica o no lo es.

--- mio.c
+++ mio.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 int main(){
-	int i,j,matrizA[3][3],matrizT[3][3];
+	int matriz[3][3]={{8, 1, 3}, {1, 7, 4}, {3, 4, 9}};
+	int i,j,esSimetrica=1;
 	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			printf("[%d][%d]:",i+1,j+1);
-			scanf("%d",&matrizA[i][j]);
-		}		
+		for(j=0;j<3;j++){	
+			if(matriz[i][j]!=matriz[j][i]){
+				esSimetrica=0;
+				break;
+			}
+		}
+		if(esSimetrica==0){
+			break;
+		}
 	}
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			matrizT[i][j]=matrizA[j][i];
-		}		
+	if(esSimetrica==0){
+		printf("La matriz no es simetrica\n");
 	}
-	
-	printf("\n\nMATRIZ A\n");
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			printf("[%d]",matrizA[i][j]);
-		}		
-		printf("\n");
+	else{
+		printf("La matriz si es simetrica\n");
 	}
-	printf("\n\nMATRIZ T\n");
-	printf("\n\n");
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			printf("[%d]",matrizT[i][j]);
-		}		
-		printf("\n");
-	}
-
 	system("pause");
 	return 0;
 
