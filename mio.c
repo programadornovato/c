975600ddc0d0f1578b8558bfdc779328caa29990
Programador Novato <info@programadornovato.com>
2024-05-08 03:34:10 -0600

    43.- Cadena de caracteres (Strings)
    En este ejercicio vamos:
    Formas de inicializar cadenas de caracteres.
    Declarar e insertar datos en nuestra cadena de caracteres.
    Desbordamiento de memoria.

--- mio.c
+++ mio.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 int main(){
-	int matriz[3][3]={{8, 1, 3}, {1, 7, 4}, {3, 4, 9}};
-	int i,j,esSimetrica=1;
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){	
-			if(matriz[i][j]!=matriz[j][i]){
-				esSimetrica=0;
-				break;
-			}
-		}
-		if(esSimetrica==0){
-			break;
-		}
-	}
-	if(esSimetrica==0){
-		printf("La matriz no es simetrica\n");
-	}
-	else{
-		printf("La matriz si es simetrica\n");
-	}
+	char programador[]="programador";
+	printf("%s\n",programador);
+    
+	char novato[]={'n','o','v','a','t','o'};
+	printf("%s\n",novato);
+
+	char nombre[5];
+	printf("Humano ingresa tu nombre y apellido:");
+	//scanf("%s",&nombre);
+	fgets(nombre,sizeof(nombre),stdin);
+	printf("%s\n",nombre);
+
 	system("pause");
 	return 0;
 
