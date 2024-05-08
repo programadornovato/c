d74f69a7f38503c8adf3a1d6ca6901cf3eb3e9e1
Programador Novato <info@programadornovato.com>
2024-05-08 16:37:44 -0600

    49.-Cambiar de minúsculas a mayúsculas con strupr
    En este ejercicio vamos:
    Pedir al humano que ingrese un texto en minúsculas o en mayúsculas.
    Pedir al humano que otro texto similar al anterior.
    Si no es la misma palabra regañar 😈 al humano de lo contrario felicitarlo.

--- mio.c
+++ mio.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 int main(){
-	char texto[40],reves[40];
-	int res;
-	regresar:
-	printf("Humano ingresa un palindromo:");
-	scanf("%s",texto);
-	strcpy(reves,texto);
-	strrev(reves);
-	res=strcmp(reves,texto);
-	if(res==0){
-		printf("Felicidades humano ahora dame la patita\n");
+    char t1[20],t2[20];
+    int res;
+    regresa:
+    printf("Humano ingresa una palabra:");
+    scanf("%s",&t1);
+    printf("Humano ingresa una palabra similar a la anterior (No sencible a mayusculas y minusculas) :");
+    scanf("%s",&t2);
+    strupr(t1);
+    strupr(t2);
+    res=strcmp(t1,t2);
+    if(res==0){
+    	printf("El texto 1 %s es igual al texto 2 %s\n ",t1,t2);
 	}else{
-		printf("Humano estupido (%s  %s) \n",reves,texto);
-		goto regresar;
+		printf("El texto 1 %s NO es igual al texto 2 %s\n ",t1,t2);
+		goto regresa;
 	}
-	
 	system("pause");
 	return 0;
 }
