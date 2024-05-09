b98d3139c00fabc72d207dd192a0b9503a3ad578
Programador Novato <info@programadornovato.com>
2024-05-09 15:03:00 -0600

    55.-Parametros en funciones y void
    Los parámetros son valores que modifican el funcionamiento de dicha función.
    Las funciones pueden recibir 1 o varios parametros de diferentes tipos de datos
    tipo nombreFuncion(tipo var) {
        instrucciones . . .
        return valor;
    }
    Void se utiliza para indicar que una función no devuelve un valor.
    void nombreFuncion(tipo var) {
        instrucciones . . .
    }

--- mio.c
+++ mio.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-int suma();
+int suma(int,int);
+void mostrarRes(int);
 int main(){
-	int resultado;
-	resultado=suma();
-	printf("Suma=%d\n",resultado);
+	int resultado,num1,num2;
+	printf("Humano dame el valor 1:");
+	scanf("%d",&num1);
+	printf("Humano dame el valor 2:");
+	scanf("%d",&num2);
+	resultado=suma(num1,num2);
+	//printf("Suma=%d\n",resultado);
+	mostrarRes(resultado);
 	system("pause");
 	return 0;
 }
-int suma(){
-	int res,n1,n2;
-	printf("Humano dame el valor 1:");
-	scanf("%d",&n1);
-	printf("Humano dame el valor 2:");
-	scanf("%d",&n2);
+int suma(int n1,int n2){
+	int res;
 	res=n1+n2;
 	return res;
+}
+void mostrarRes(int res){
+	printf("El resultado=%d\n",res);
 }
