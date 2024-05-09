943df51e519ef58ec9c9be7b7f7ad872a514844d
Programador Novato <info@programadornovato.com>
2024-05-09 01:30:43 -0600

    53.-Búsqueda Secuencial
    En informática, la búsqueda lineal o la búsqueda secuencial es un método para encontrar un valor objetivo dentro de una lista.Ésta comprueba secuencialmente cada elemento de la lista para el valor objetivo hasta que es encontrado o hasta que todos los elementos hayan sido comparados.
    
    Pedir al humano que ingrese un número.
    Buscar el numero que ingreso el humano en la lista.
    Si el numero no esta en la lista pedir al humano que ingrese otro numero.

--- mio.c
+++ mio.c
@@ -1,38 +1,32 @@
-//BURBUJA NORMAL = 20 CICLOS
-//BURBUJA MEJORADO = 20 CICLOS
 #include <stdio.h>
 #include <stdbool.h>
 int main(){
-	int i,j,aux=0,n[5],ciclos=0;
-	bool ordenado=false;
+	int lista[5]={1,2,3,4,5};
+	int n,i;
+	regresa:
+	printf("Escribe algun elemento de esta lista:\n");
 	for(i=0;i<5;i++){
-		printf("Ingresa el valor %d:",i+1);
-		scanf("%d",&n[i]);
+		printf("%d ",lista[i]);
 	}
-	
-	for(i=0;i<5 && ordenado==false;i++){
-		ordenado=true;
-		for(j=0;j<4;j++){
-			if(n[j]>n[j+1]){
-				aux=n[j];
-				n[j]=n[j+1];
-				n[j+1]=aux;
-				ordenado=false;
-			}
-			ciclos++;
-			
+	printf(":");
+	scanf("%d",&n);
+	i=0;
+	int pos=0;
+	bool encontrado=false;
+	while(i<5 && encontrado==false ){
+		if(lista[i]==n){
+			pos=i;
+			encontrado=true;
+			break;
 		}
+		i++;
 	}
-	printf("\nESTOS SON TUS NUMEROS ORDENADOS ACENDENTE EN %d CICLOS \n",ciclos);
-	for(i=0;i<5;i++){
-		printf("%d\n",n[i]);
+	if(encontrado==true){
+		printf("\nMuy bien humano encontramos el elemento en la posicion %d\n",pos+1);
+	}else{
+		printf("\nHumano estupido intentalo de nuevo\n");
+		goto regresa;
 	}
-
-	printf("\nESTOS SON TUS NUMEROS ORDENADOS DECENDENTE\n");
-	for(i=4;i>=0;i--){
-		printf("%d\n",n[i]);
-	}
-		
 	system("pause");
 	return 0;
 }
