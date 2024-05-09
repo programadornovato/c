be0c719a175da126364005193d88e491cba00f80
Programador Novato <info@programadornovato.com>
2024-05-09 16:36:16 -0600

    57.-Variables globales
    Una variable global es aquella que se declara fuera de cualquier función o bloque de código. Esto significa que la variable es accesible desde cualquier parte del programa, una vez que ha sido declarada.
    
    Crearemos la función pideNumeros que no retorna ningún valor (void) y tampoco acepta argumentos.
    Creamos una función llamada sumaNumeros que calculará la suma de numero1 y numero2.
    En la función principal llamamos a pideNumeros, sumaNumeros, mostraremos el resultado.

--- mio.c
+++ mio.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-int retornaMulti(int,int);
-void muestraMulti(int,int);
+int n1,n2;
+void pideNumeros();
+void mustraResultado();
 int main() {
-	int d1,d2;
-	printf("Dato 1:");
-	scanf("%d",&d1);
-	printf("Dato 2:");
-	scanf("%d",&d2);
-	int res=retornaMulti(d1,d2);
-	printf("La multiplicacion es %d\n",res);
-	muestraMulti(d1,d2);
-    system("pause");
+	pideNumeros();
+	mustraResultado();
+	system("pause");	
     return 0;
 }
 
-int retornaMulti(int n1,int n2){
-	/*
-	int res;
-	res=n1*n2;
-	return res;
-	*/
-	return n1*n2;
+void pideNumeros(){
+	printf("Humano ingresa el valor 1:");
+	scanf("%d",&n1);
+	printf("Humano ingresa el valor 2:");
+	scanf("%d",&n2);
 }
-void muestraMulti(int n1,int n2){
-	/*
+void mustraResultado(){
 	int res;
-	res=n1*n2;
-	printf("La multiplicacion es %d\n",res);
-	*/
-	printf("La multiplicacion es %d\n",n1*n2);
+	res=n1+n2;
+	printf("La suma de %d + %d = %d\n",n1,n2,res);
 }
