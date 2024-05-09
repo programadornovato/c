4bf644845bd0eb43c5568b16109e71f1a57ba154
Programador Novato <info@programadornovato.com>
2024-05-08 20:53:09 -0600

    53.-Ordenamiento método burbuja mejorado
    En este ejercicio vamos:
    Si los la lista de números ya está ordenada ya no hay necesidad de seguir ordenando y hay que terminar el ciclo.

--- mio.c
+++ mio.c
@@ -1,22 +1,29 @@
+//BURBUJA NORMAL = 20 CICLOS
+//BURBUJA MEJORADO = 20 CICLOS
 #include <stdio.h>
+#include <stdbool.h>
 int main(){
-	int i,j,aux=0,n[5];
+	int i,j,aux=0,n[5],ciclos=0;
+	bool ordenado=false;
 	for(i=0;i<5;i++){
 		printf("Ingresa el valor %d:",i+1);
 		scanf("%d",&n[i]);
 	}
 	
-	for(i=0;i<5;i++){
+	for(i=0;i<5 && ordenado==false;i++){
+		ordenado=true;
 		for(j=0;j<4;j++){
 			if(n[j]>n[j+1]){
 				aux=n[j];
 				n[j]=n[j+1];
 				n[j+1]=aux;
-				
+				ordenado=false;
 			}
+			ciclos++;
+			
 		}
 	}
-	printf("\nESTOS SON TUS NUMEROS ORDENADOS ACENDENTE \n");
+	printf("\nESTOS SON TUS NUMEROS ORDENADOS ACENDENTE EN %d CICLOS \n",ciclos);
 	for(i=0;i<5;i++){
 		printf("%d\n",n[i]);
 	}
