a8b61cc2cb8aadf3da4d5e7ddfdca1e94eff5aea
Programador Novato <info@programadornovato.com>
2024-05-09 19:09:48 -0600

    58.-Paso de parámetros por referencia
    Las variables tienen un scope (área de alcance) pero este scope se puede romper usando las referencias que nos permiten compartir la variable y su posición de memoria, de tal suerte que podamos manipular las variables desde cualquier punto de nuestro programa.
    
    int main(){
        cambaValor(&num1, &num2);
    }
    void cambaValor(int* val1, int* val2){
        *val1 = 10;
        *val2 = 20;
    }

--- mio.c
+++ mio.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-int n1,n2;
-void pideNumeros();
-void mustraResultado();
-int main() {
-	pideNumeros();
-	mustraResultado();
-	system("pause");	
+void cambiaValor(int*,int*);
+int main(){
+	int val1,val2;
+	printf("Valor 1:");
+	scanf("%d",&val1);
+	printf("Valor 2:");
+	scanf("%d",&val2);
+	
+	printf("Valor 1:%d\n",val1);
+	printf("Valor 2:%d\n",val2);
+	
+	cambiaValor(&val1,&val2);
+	printf("\n");
+	printf("Valor 1:%d\n",val1);
+	printf("Valor 2:%d\n",val2);
+	
+    system("pause");
     return 0;
 }
+void cambiaValor(int* n1,int* n2){
+	*n1=5;
+	*n2=6;
+	
+}
 
-void pideNumeros(){
-	printf("Humano ingresa el valor 1:");
-	scanf("%d",&n1);
-	printf("Humano ingresa el valor 2:");
-	scanf("%d",&n2);
-}
-void mustraResultado(){
-	int res;
-	res=n1+n2;
-	printf("La suma de %d + %d = %d\n",n1,n2,res);
-}
